2a6d3e368d042b24a696430b14ac65b331818dec
Shayan704 <24418296-Shayan704@users.noreply.replit.com>
2023-07-29 16:56:54 +0000

    new comment

--- main.py
+++ main.py
@@ -62,7 +62,7 @@
 import random
 from art import logo
 from replit import clear
-
+# comment
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 game_cont=input("Do you want to play the game of BLACKJACK? Type 'y' or 'n'. ").lower()
 while(game_cont=='y'):
